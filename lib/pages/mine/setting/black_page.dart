a0697324a07de4d1c3899f6722e279d4e4580bdf
Your Name <you@example.com>
2023-06-15 11:24:32 +0800

    新增设置功能

---
+++ lib/pages/mine/setting/black_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+/// 黑名单
+class BlackPage extends StatefulWidget {
+  const BlackPage({Key? key}) : super(key: key);
+
+  @override
+  State<BlackPage> createState() => _BlackPageState();
+}
+
+class _BlackPageState extends State<BlackPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
