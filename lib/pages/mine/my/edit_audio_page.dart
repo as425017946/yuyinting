a99515b844bcc6fa799a34527bb89a6b371ed0cb
xiaobin <425017946@qq.com>
2024-02-02 15:52:57 +0800

    0

--- lib/pages/mine/my/edit_audio_page.dart
+++ lib/pages/mine/my/edit_audio_page.dart
@@ -597,7 +597,7 @@ class _EditAudioPageState extends State<EditAudioPage> {
 
   /// 获取文件url
   Future<void> doPostPostFileUpload(path) async {
-
+    var directory = await PathProviderPlatform.instance.getApplicationSupportPath();
     LogE('音频上传///$path');
     Loading.show("音频上传中...");
     var name = path.substring(path.lastIndexOf("/") + 1, path.length);
