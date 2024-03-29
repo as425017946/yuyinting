9d7a80b02334be03cd5f234b5c2ba5d000fa5fce
hanyz <hanyzjob@163.com>
2024-03-29 15:23:17 +0800

    物理返回取消loading

--- lib/widget/SwiperPage.dart
+++ lib/widget/SwiperPage.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:yuyinting/utils/loading.dart';
 import 'package:yuyinting/utils/log_util.dart';
 
 import '../utils/my_utils.dart';
@@ -57,48 +58,55 @@ class _SwiperPageState extends State<SwiperPage> {
         ],
       ),
       backgroundColor: Colors.black,
-      body: GestureDetector(
-        behavior: HitTestBehavior.translucent,
-        onTap: (() {
-          //点击任意地方都能关闭页面，并且不影响你的滑动查看
-          Navigator.pop(context);
-        }),
-        child: Swiper(
-          itemBuilder: (BuildContext context, int index) {
-            return SizedBox(
-                width: double.infinity,
-                child: (widget.imgList[index].contains('com.leimu.yuyinting') ||
-                        widget.imgList[index].contains('storage'))
-                    ? Image.file(
-                        File(widget.imgList[index].toString()),
-                        fit: BoxFit.contain,
-                        gaplessPlayback: true,
-                      )
-                    : FadeInImage.assetNetwork(
-                        placeholder: 'assets/images/img_placeholder.png',
-                        image: widget.imgList[index],
-                        fit: BoxFit.contain,
-                        imageErrorBuilder: (context, error, stackTrace) {
-                          // TODO 图片加载错误后展示的 widget
-                          // print("---图片加载错误---");
-                          // 此处不能 setState
-                          return WidgetUtils.showImages(
-                            'assets/images/img_placeholder.png',
-                            double.infinity,
-                            double.infinity,
-                          );
-                        },
-                      ));
-          },
-          itemCount: widget.imgList.length,
-          pagination: const SwiperPagination(),
-          loop: false, //下面的分页小点
-          onIndexChanged: (value) {
-            setState(() {
-              current = value;
-            });
-          },
+      body: WillPopScope(
+        onWillPop: () async {
+          Loading.dismiss();
+          return true;
+        },
+        child: GestureDetector(
+          behavior: HitTestBehavior.translucent,
+          onTap: (() {
+            //点击任意地方都能关闭页面，并且不影响你的滑动查看
+            Navigator.pop(context);
+          }),
+          child: Swiper(
+            itemBuilder: (BuildContext context, int index) {
+              return SizedBox(
+                  width: double.infinity,
+                  child:
+                      (widget.imgList[index].contains('com.leimu.yuyinting') ||
+                              widget.imgList[index].contains('storage'))
+                          ? Image.file(
+                              File(widget.imgList[index].toString()),
+                              fit: BoxFit.contain,
+                              gaplessPlayback: true,
+                            )
+                          : FadeInImage.assetNetwork(
+                              placeholder: 'assets/images/img_placeholder.png',
+                              image: widget.imgList[index],
+                              fit: BoxFit.contain,
+                              imageErrorBuilder: (context, error, stackTrace) {
+                                // TODO 图片加载错误后展示的 widget
+                                // print("---图片加载错误---");
+                                // 此处不能 setState
+                                return WidgetUtils.showImages(
+                                  'assets/images/img_placeholder.png',
+                                  double.infinity,
+                                  double.infinity,
+                                );
+                              },
+                            ));
+            },
+            itemCount: widget.imgList.length,
+            pagination: const SwiperPagination(),
+            loop: false, //下面的分页小点
+            onIndexChanged: (value) {
+              setState(() {
+                current = value;
+              });
+            },
 //        control: new SwiperControl(),  //左右的那个箭头,在某模拟器中会出现蓝线
+          ),
         ),
       ),
     );
