ba984e79bae5f2990feeb7ad43970340101c236c
hanyz <hanyzjob@163.com>
2024-04-17 17:47:54 +0800

    roomID

--- lib/pages/home/shoucang_page.dart
+++ lib/pages/home/shoucang_page.dart
@@ -358,7 +358,7 @@ class _ShoucangPageState extends State<ShoucangPage>
   /// 加入房间前
   Future<void> doPostBeforeJoin(roomID) async {
     //判断房间id是否为空的
-    if (sp.getString('roomID') == null || sp.getString('').toString().isEmpty) {
+    if (sp.getString('roomID') == null || sp.getString('roomID').toString().isEmpty) {
       return;
     } else {
       // 不是空的，并且不是之前进入的房间
