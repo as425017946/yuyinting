7a40683cc081e06f9586fd59a28f7153c34712ba
hanyz <hanyzjob@163.com>
2024-04-22 11:18:32 +0800

    头像圆角

--- lib/utils/svga_tools.dart
+++ lib/utils/svga_tools.dart
@@ -1,11 +1,19 @@
-
+import 'dart:math';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:svgaplayer_flutter/svgaplayer_flutter.dart';
 
 extension SvgaTools on MovieEntity {
 
   Future<void> hfItem(String img, String name) async {
-    await dynamicItem.setImageWithUrl(img, '03');
+    // await dynamicItem.setImageWithUrl(img, '03');
+    final image = await _getNetImage(img);
+    dynamicItem.setDynamicDrawer((canvas, frameIndex) {
+      canvas.clipPath(Path()..addOval(Rect.fromCircle(center: const Offset(30, 30), radius: 30)));
+      final size = min(image.width, image.height).toDouble();
+      canvas.drawImageRect(image, Rect.fromLTWH((image.width - size)/2, (image.height - size)/2, size, size), const Rect.fromLTWH(0, 0, 60, 60), Paint());
+    }, '03');
     dynamicItem.setDynamicDrawer((canvas, frameIndex) {
       final textPainter = TextPainter(
         text: TextSpan(text: name, style: const TextStyle(fontSize: 30, color: Colors.yellow)),
@@ -13,6 +21,7 @@ extension SvgaTools on MovieEntity {
         maxLines: 1,
       )..layout(maxWidth: 192);
       textPainter.paint(canvas, const Offset(15, -2.5));
+      
     }, '01');
     dynamicItem.setDynamicDrawer((canvas, frameIndex) {
       final textPainter = TextPainter(
@@ -22,5 +31,12 @@ extension SvgaTools on MovieEntity {
       )..layout(maxWidth: 192);
       textPainter.paint(canvas, const Offset(15, 0));
     }, '02');
+  }
+
+  Future<ui.Image> _getNetImage(String url, {width, height}) async {
+    ByteData data = await NetworkAssetBundle(Uri.parse(url)).load(url);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width, targetHeight: height);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return fi.image;
   }
 }
