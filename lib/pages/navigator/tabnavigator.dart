11b71c706171fa4c6db25368372a34fef3157629
xiaobin <425017946@qq.com>
2024-04-29 14:37:17 +0800

    新增底部按钮

--- lib/pages/navigator/tabnavigator.dart
+++ lib/pages/navigator/tabnavigator.dart
@@ -807,7 +807,7 @@ class _Tab_NavigatorState extends State<Tab_Navigator>
           isRed
               ? Positioned(
                   bottom: (isDevices == 'ios' ? 105 : 65) * 1.3 / 2,
-                  right: 295.w,
+                  right: 195.w,
                   child: Container(
                     width: 15 * 1.3 / 2,
                     height: 15 * 1.3 / 2,
