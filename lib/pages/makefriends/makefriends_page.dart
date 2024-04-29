1fefadbf59cb6a671586b9d741b7a6f19f56d0b2
hanyz <hanyzjob@163.com>
2024-04-29 14:37:55 +0800

    no message

--- lib/pages/makefriends/makefriends_page.dart
+++ lib/pages/makefriends/makefriends_page.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+import 'makefriends_model.dart';
 
 class MakefriendsPage extends StatelessWidget {
+  final MakefriendsController c = Get.put(MakefriendsController());
+   MakefriendsPage( {super.key});
+  
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Text('data');
+    return Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/all_bg.png"),
+            fit: BoxFit.fill, 
+          ),
+        ),
+    );
   }
 }
