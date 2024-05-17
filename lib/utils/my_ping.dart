b9e8856e30ab45edcc75fb1ab5c143c19b214fa8
hanyz <hanyzjob@163.com>
2024-05-17 13:49:51 +0800

    去掉ping

--- lib/utils/my_ping.dart
+++ lib/utils/my_ping.dart
@@ -4,23 +4,23 @@ import 'log_util.dart';
 
 class MyPing {
   static checkIp(ips, void Function(String) callBack) {
-    int? time;
-    for (var ip in ips) {
-      final ping = Ping(ip, count: 3);
-      ping.stream.listen(
-        (event) {
-          if (event.summary != null) {
-            final s = event.summary!.time!.inMilliseconds;
-            // ignore: unnecessary_brace_in_string_interps
-            LogE("Ping：${ip} ${s}ms");
-            if (time == null || time! > s) {
-              time = s;
-              callBack(ip);
-            }
-          }
-          LogE('Running command: $event');
-        },
-      );
-    }
+    // int? time;
+    // for (var ip in ips) {
+    //   final ping = Ping(ip, count: 3);
+    //   ping.stream.listen(
+    //     (event) {
+    //       if (event.summary != null) {
+    //         final s = event.summary!.time!.inMilliseconds;
+    //         // ignore: unnecessary_brace_in_string_interps
+    //         LogE("Ping：${ip} ${s}ms");
+    //         if (time == null || time! > s) {
+    //           time = s;
+    //           callBack(ip);
+    //         }
+    //       }
+    //       LogE('Running command: $event');
+    //     },
+    //   );
+    // }
   }
 }
